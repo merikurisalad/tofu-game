a6448bdae8e7a48e9ad0aec5ac4b3d297e3bbc4a
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-08-12 17:16:49 -0700

    feat: add random movement, jumping

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,31 +3,84 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
+    private float idleTime = 5f;
+    private float idleTimer = 0f;
+    private float runTime = 5f;
+    private float runTimer = 0f;
+    private float jumpTime = 5f;
+    private float jumpTimer = 0f;
+    private bool isIdle = true;
+    private bool isJump = false;
     private float direction = 1f; // Current movement direction (1 = right, -1 = left)
-    SpriteRenderer TofuSprite;
-    Animator TofuAnimator;
+    private SpriteRenderer TofuSprite;
+    private Animator TofuAnimator;
 
     // Update is called once per frame
     void Update()
     {
-        
-        // Move the object horizontally
-        transform.Translate(Vector2.right * 0.5f * direction * Time.deltaTime);
-
-        // Check if the object reaches the boundaries and change the direction
-        if (transform.position.x <= -2f)
+       if (Input.GetKey("space"))
+       {
+            if (!isJump)
+            {
+                isJump = true;
+                TofuAnimator.Play("tofu_jump");
+                jumpTime = 1f;
+            }
+       }
+       
+       if (isJump)
         {
-            direction = 1f; // Change to move right
-            Vector3 NewScale = transform.localScale;
-            NewScale.x *= -1;
-            transform.localScale = NewScale;
+            jumpTimer += Time.deltaTime;
+            if (jumpTimer >= jumpTime)
+            {
+                jumpTimer = 0f;
+                runTimer = 0f;
+                isJump = false;
+                isIdle = true;
+                idleTime = Random.Range(3f, 8f);
+                idleTimer = 0f;
+                TofuAnimator.Play("tofu_idle");
+            }
         }
-        else if (transform.position.x >= 2f)
+        else if (isIdle)
         {
-            direction = -1f; // Change to move left
-            Vector3 NewScale = transform.localScale;
-            NewScale.x *= -1;
-            transform.localScale = NewScale;
+            idleTimer += Time.deltaTime;
+            if (idleTimer >= idleTime)
+            {
+                idleTimer = 0f;
+                isIdle = false;
+                runTime = Random.Range(3f, 8f);
+                TofuAnimator.Play("tofu_walk");
+            }
+        } 
+        else 
+        {
+            runTimer += Time.deltaTime;
+            if (runTimer >= runTime)
+            {
+                runTimer = 0f;
+                isIdle = true;
+                idleTime = Random.Range(3f, 8f);
+                TofuAnimator.Play("tofu_idle");
+            }
+            // Move the object horizontally
+            transform.Translate(Vector2.right * 0.5f * direction * Time.deltaTime);
+
+            // Check if the object reaches the boundaries and change the direction
+            if (transform.position.x <= -2f)
+            {
+                direction = 1f; // Change to move right
+                Vector3 NewScale = transform.localScale;
+                NewScale.x *= -1;
+                transform.localScale = NewScale;
+            }
+            else if (transform.position.x >= 2f)
+            {
+                direction = -1f; // Change to move left
+                Vector3 NewScale = transform.localScale;
+                NewScale.x *= -1;
+                transform.localScale = NewScale;
+            }
         }
     }
 
@@ -35,7 +88,7 @@ public class Movement : MonoBehaviour
     {
         TofuSprite = this.GetComponent<SpriteRenderer>();
         TofuAnimator = this.GetComponent<Animator>();
-        TofuAnimator.Play("tofu_walk");
-
+        TofuAnimator.Play("tofu_idle");
+        isIdle = true;
     }
 }
