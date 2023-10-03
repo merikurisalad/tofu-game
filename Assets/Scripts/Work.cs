79a0e95ec30c0ae5392100f6876da3563a4f055c
HS940405 <hansol.yang.94@gmail.com>
2023-10-02 17:03:08 -0700

    fixed: int related to status/money to double

--- Assets/Scripts/Work.cs
+++ Assets/Scripts/Work.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Work : Activities
 {
     // Start is called before the first frame update
-    private const int ACTION_UNIT_1ST = 1;
-    private const int ACTION_UNIT_2ND = 1.1;
-    private const int ACTION_UNIT_3RD = 1.2;
+    private const double ACTION_UNIT_1ST = 1.0;
+    private const double ACTION_UNIT_2ND = 1.1;
+    private const double ACTION_UNIT_3RD = 1.2;
     void Start()
     {
         
@@ -22,8 +22,8 @@ public class Work : Activities
     void CollectingRecycles()
     {
         System.Random rand = new System.Random();
-        int[] possibleAmount = new int[] { 5, 10, 15 };
-        int earnedMoney = possibleAmount[rand.Next(0, 2)];
+        double[] possibleAmount = double int[] { 3.0, 4.0, 5.0 };
+        double earnedMoney = possibleAmount[rand.Next(0, 2)];
         base.DoAction(affection=-(ACTION_UNIT_1ST*0.5), intelligence=(ACTION_UNIT_1ST*0.5), money=earnedMoney);
     }
 }
